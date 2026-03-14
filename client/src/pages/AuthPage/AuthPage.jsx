66d554024189a261b356bf2bdf4561b8eb2484e5
Muzaffar Ameen <muzaffarameen389@gmail.com>
2026-03-14 06:38:01 +0500

    fix AuthPage to use axiosConfig

--- client/src/pages/AuthPage/AuthPage.jsx
+++ client/src/pages/AuthPage/AuthPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import "./AuthPage.css";
-import axios from "axios";
+import axios from "../../api/axiosConfig";
 
 const EyeIcon = ({ open }) => (
   <svg
